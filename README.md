5a970683b01fb54de34be5f1653e9dc1a0d8965f
Diego Marzo <diegomarzo@gmail.com>
2022-11-10 09:49:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Svelte Components Consumer
+
+This is just a simple playground to see how we use the Svelte Components
+
---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import WelcomeMessage from "@diegomarzo/svelte-components"
+  import StylishButton from "@diegomarzo/svelte-components"
+</script>
+
+<main>
+  <WelcomeMessage name="World"/>
+  <StylishButton bgColor="pink" txtColor="darkblue" message="I'm very stylish"></StylishButton>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
